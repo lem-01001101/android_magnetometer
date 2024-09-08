c072c71f466599ea715e7523d279905c43085382
lem-01001101 <leo.magtibay.dev@gmail.com>
2024-09-08 12:16:24 -0700

    first commit

---
+++ app/src/main/java/com/example/compass_magnetometer_test/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.compass_magnetometer_test
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity(), SensorEventListener {
+
+    private var accelerometer: Sensor? = null
+    private var magnetometer: Sensor? = null
+    private lateinit var sensorManager: SensorManager
+    private lateinit var rotationTV: TextView
+
+    private val gravity = FloatArray(3)
+    private val geomagnetic = FloatArray(3)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+
+        rotationTV = findViewById(R.id.textView)
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, gravity, 0, event.values.size)
+        } else if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, geomagnetic, 0, event.values.size)
+        }
+
+        val R = FloatArray(9)
+        val I = FloatArray(9)
+        if (SensorManager.getRotationMatrix(R, I, gravity, geomagnetic)) {
+            val orientation = FloatArray(3)
+            SensorManager.getOrientation(R, orientation)
+            val degree = Math.toDegrees(orientation[0].toDouble()).toFloat()
+
+            rotationTV.text = String.format("%.1f degrees", degree)
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI)
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this, accelerometer)
+        sensorManager.unregisterListener(this, magnetometer)
+    }
+}
